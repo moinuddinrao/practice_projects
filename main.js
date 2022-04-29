42b61fba4af1fbd44d1ee96165199970bbd48ca3
Moin ud din <74861094+moinuddinrao@users.noreply.github.com>
2022-04-29 23:59:44 +0500

    navbar title hadith sections

---
+++ main.js
@@ -0,0 +1,59 @@
+var counter = 1;
+setInterval(function () {
+  document.getElementById("radio" + counter).checked = true;
+  counter++;
+  if (counter > 3) {
+    counter = 1;
+  }
+}, 4000);
+
+// <!--******************
+// Counter Animation
+// ******************-->
+const counters = document.querySelectorAll(".counter");
+const speed = 1000; // The lower the slower
+
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+
+    // Lower inc to slow and higher to slow
+    const inc = target / speed;
+
+    // console.log(inc);
+    // console.log(count);
+
+    // Check if target is reached
+    if (count < target) {
+      // Add inc to count and output in counter
+      counter.innerText = Math.ceil(count + inc);
+      // Call function every ms
+      setTimeout(updateCount, 1);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCount();
+});
+//**************************************************
+
+// ***********************
+// Profile slide Animation
+// ***********************
+
+var pcounter = 1;
+setInterval(function () {
+  document.getElementById("pradio" + pcounter).checked = true;
+  pcounter++;
+  if (pcounter > 4) {
+    pcounter = 1;
+  }
+}, 2000);
+
+//**************************************************
+
+// ***********************
+// Partners slide Animation
+// ***********************
