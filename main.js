992b285ca53923bcaf423cec7577a53d1663f949
Moin ud din <74861094+moinuddinrao@users.noreply.github.com>
2022-05-01 15:42:18 +0500

    fixed event-img

--- main.js
+++ main.js
@@ -29,14 +29,17 @@ counters.forEach((counter) => {
   updateCount();
 });
 
+
+
+
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-10vh";
-  }
-  prevScrollpos = currentScrollPos;
-}
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-10vh";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
